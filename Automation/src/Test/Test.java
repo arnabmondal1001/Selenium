c7397b8a87711b304db3b98a89f7481259c39c38
arnabmondal1001 <52192693+arnabmondal1001@users.noreply.github.com>
2019-06-25 12:31:17 +0530

    Update Test.java
    This is changed from GITHUB

--- Automation/src/Test/Test.java
+++ Automation/src/Test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is my First Hithub Project");
+		System.out.println("This is my First Hithub Project and Checking in from Web");
 	}
 
 }
